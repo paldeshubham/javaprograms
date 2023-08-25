c74096667893dda12d50dcdcc5d2939d9777f445
Shubham Palde <114591652+paldeshubham@users.noreply.github.com>
2023-08-25 13:53:45 +0530

    Add files via upload

---
+++ Cube.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Cube
+{
+	public static void main(String[] args)
+	{
+		Scanner sc= new Scanner(System.in);
+		System.out.print("ENTER THE VALUE OF A:");
+		int a= sc.nextInt();
+		System.out.println("THE CUBE OF A IS:"+(a*a*a));
+	}
+}
