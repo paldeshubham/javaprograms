2a205737014ee97c5c1728e01534bc07e04032f7
Shubham Palde <114591652+paldeshubham@users.noreply.github.com>
2023-09-01 13:02:40 +0530

    Add files via upload

---
+++ Com.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Com{
+		public static void main(String[] args)
+		{
+			int n=5;
+			int i,j;
+			for(i=1;i<=n;i++)
+			{
+				for (j=1;j<=i;j++)
+				{
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+		}
+}
