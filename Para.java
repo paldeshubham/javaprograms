a4e1005161ede00d5883e4c46a3ff17f8a17ead5
Shubham Palde <114591652+paldeshubham@users.noreply.github.com>
2023-09-08 14:32:54 +0530

    Add files via upload
    yes

---
+++ Para.java
@@ -0,0 +1,13 @@
+   // java program of parametrized constructor
+   class Para{
+   	// creating parameterized constructor
+   	Para(int a,int b)
+   	{
+   		System.out.println("Addition is:"+(a+b));
+   	}
+   	public static void main(String args[])
+   	{
+   		//calling parameterized constructor
+   		Para p1= new Para(6,7);
+   	}
+   }
