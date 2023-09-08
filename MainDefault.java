24cb5a507a3cafa8e161a385f56fb95807b84a51
Shubham Palde <114591652+paldeshubham@users.noreply.github.com>
2023-09-08 14:27:18 +0530

    Add files via upload
    yes

---
+++ MainDefault.java
@@ -0,0 +1,13 @@
+class MainDefault{
+	 int a=2,b=3;
+	 // creating a default ocnstructor
+	MainDefault()
+	 {
+	 	System.out.println("Addition is:"+(a+b));
+	 }
+
+	 public static void main(String[] args){
+		MainDefault d1=new MainDefault();
+			 
+	 }
+}
