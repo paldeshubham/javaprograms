0e88419c6d4a4f853bceaa6abab17815834edafd
Shubham Palde <114591652+paldeshubham@users.noreply.github.com>
2023-10-06 11:32:44 +0530

    "this" keyword program for addition of 2 numbers

--- Test.java
+++ Test.java
@@ -1,36 +1,25 @@
-// creating a claaa
+ class Add {
+    int a;
+    int b;
 
-//class 1 (parent class)
- class Poly{
- 	void print(){
-		System.out.println("This is main class");
-	}
-}	
-	
-// this is class 2
-class Sub1 extends Poly {
-	void print(){
-	System.out.println("This is sub class 1");
-	}
+    Add(int a,int b)
+    {
+        this.a=a;
+        this.b=b;
+
+
+    }
+    void display()
+    {
+        System.out.println("addition of aand b is:"+(a+b));
+    }
+    
 }
-
-// this is class 3
-class Sub2 extends Poly {
-	void print(){
-	System.out.println("This is sub class 2");
- 	}
-}
-
-// main class is now here from where the code will be execute
 class Test{
-	public static void main(String[] args)
-	{ 	
-		//creating object to the class
-		Poly a;
-		 a = new Sub1();
-		a.print();
-		
-		a= new Sub2();
-		a.print();
-	}
+    public static void main(String[] args)
+    {
+        Add A1=new Add(2,3);
+        A1.display();
+    }
+
 }
