fabb44ce469a7db0b81262a9305944f9062c17f9
Shubham Palde <114591652+paldeshubham@users.noreply.github.com>
2023-10-27 20:48:26 +0530

    Add files via upload
    generic code

---
+++ JAVA ASSIGNMENTS/Generic_code.java
@@ -0,0 +1,90 @@
+import java.util.*;
+public class Generic_code {
+        public static void main(String[] args) {
+            int ch;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("1.check even or odd");
+            System.out.println("2.check prime or not");
+            System.out.println("check palindrome or not");
+            System.out.println("Enter your choice:");
+            ch=sc.nextInt();
+            switch (ch) {
+                case 1://even or not
+                    int n;
+                    System.out.println("enter the number you want to check:");
+                    n=sc.nextInt();
+                    ArrayList<Integer> number = new ArrayList<>(); 
+                    number.add(n);
+                    System.out.println(number);
+                    if(n%2==0)
+                    {
+                        System.out.println(number+" is an even number");
+                    }
+                    else
+                    {
+                        System.out.println(number+"is an odd number");
+                    }
+                    
+                    break;
+                case 2://prime or not
+                    ArrayList<Integer> number1 =new ArrayList<>();
+                    System.out.print("Enter the value of a:");
+                    int a=sc.nextInt();
+                    number1.add(a);
+                    int i=2;
+                    boolean flag=false;
+                    for(i=2;i<=a;++i)
+                    {
+                        if (a%2==0)
+                        {
+                            flag=true;
+                            break;
+                        }
+                    }
+                    if(flag!=true)
+                    {
+                        System.out.println(number1+"is a is prime number");
+                    }
+                    else
+                    {
+                        System.out.println(number1+"is a prime number");	
+                    }
+                    break;
+
+                case 3://palindrome or not
+                    int num,r,temp;
+                    ArrayList<Integer> number2 = new ArrayList<>();
+                    int sum=0;
+                    System.out.println("Enter the number to be check which is palindrome or not:");
+                    num=sc.nextInt();
+                    number2.add(num);
+
+                    temp=num;
+                    
+                    while(num>0)
+                    {
+                        r=num%10;
+                        sum=(sum*10)+r;
+                        num=num/10;
+                    }
+
+                    if(temp==sum)
+                    {
+                        System.out.println(number2+" is a palindrome number");
+                    }
+                    else
+                    {
+                        System.out.println(number2+" is not a palindrome");
+                    }
+                    break;
+                    
+                    
+
+            
+                default:
+                    break;
+            }
+        }
+
+    
+}
